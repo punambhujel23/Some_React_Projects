712cde52f2e958f1ebb8112a766eb08f92c12d2e
punambhujel23 <punam.023222@gandakiuniversity.edu.np>
2025-07-13 21:02:46 +0545

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("The sum of a & b is: " + sum);
+    }
+}
